51ac6d62df10b9e441e0c19422e97cd36e4cd062
ibaragozz <baragozz1@gmail.com>
2024-10-20 19:24:29 +0300

    update main and index

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather = None
+    if request.method == 'POST':
+        city = request.form['city']
+        weather =get_weather(city)
+
+    return render_template('index.html', weather=weather)
+
+def get_weather(city):
+    api_key = '41b7b468dac246d2f135135a6ba0a843'
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    response = requests.get(url)
+    return response.json()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
